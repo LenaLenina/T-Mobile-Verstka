e79aed7de50947f031843fa9b32f7d35359307df
LenaLenina <lenalenina5677@gmail.com>
2019-02-06 21:18:44 +0300

    Добавьте файлы проекта.

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $('.header').html("<h1>Hello world</h1>");
+    $('.removed_div').click(function () {
+        $(this).remove();
+        $('.header').remove();
+    })
+});
+
+$(document).on('click', 'h1', function () {
+    $(this).append("</br>Hello too");
+    $(this).css('color', 'red');
+});
---
+++ assets/js/script2.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $('body').css('border', '1px solid red');
+    $('.header').css("border", "1px solid blue");
+    $('#main_nav').css("border", "1px solid green");
+    $('#main_nav li a').css('color', 'red');
+    $('ul>li').css("border", "1px solid black");
+    $('a[href=#home]').css('font-weight', 'bold');
+});
